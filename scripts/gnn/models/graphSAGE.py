49f68c94dcf331df65113330ff9feec5ed234e54
nkt1313 <ankit.basu@tum.de>
2025-08-01 21:41:21 +0200

    included SageCONV and modified with graphnorm and residual blocks

--- scripts/gnn/models/graphSAGE.py
+++ scripts/gnn/models/graphSAGE.py
@@ -5,7 +5,7 @@ import tqdm as tqdm
 import wandb
 import torch
 from torch import nn
-from torch_geometric.nn import SAGEConv
+from torch_geometric.nn import SAGEConv, GraphNorm
 from torch_geometric.utils import add_self_loops
 
 import torch
@@ -27,17 +27,21 @@ class GraphSAGE(BaseGNN):
     Uses a simple ReLU-based update function with an option for a learnable MLP-based update.
     """
     def __init__(self, 
-                 in_channels: int = 5, 
+                 in_channels: int = 5,
+                 use_pos: bool = False,
                  out_channels: int = 1,
-                 hidden_channels: list[int] = [256, 256],
-                 aggregator: str = 'max',  # Options: 'mean' | 'gcn' | 'pool' | 'max'
-                 update_function: str = 'mlp',  # Options: 'relu', 'mlp'
-                 mlp_hidden_dim: int = 1024,  # Hidden dim for learnable MLP update
+                 hidden_channels: list[int] = [256,512],
                  dropout: float = 0.3, 
-                 use_dropout: bool = True,
+                 use_dropout: bool = False,
                  predict_mode_stats: bool = False,
                  dtype: torch.dtype = torch.float32,
-                 log_to_wandb: bool = True):
+                 log_to_wandb: bool = True,
+                 use_graph_norm: bool = False,
+                 use_residuals: bool = False,
+                 #GraphSAGE specific parameters
+                 aggregator: str = 'mean',  # Options: 'max' | 'mean'
+                 update_function: str = 'relu',  # Options: 'relu' | 'mlp'
+                 mlp_hidden_dim: int = 1024):  # Hidden dim for learnable MLP update
                 
         # Call parent class constructor
         super().__init__(
@@ -52,22 +56,26 @@ class GraphSAGE(BaseGNN):
         # Model-specific parameters
         self.hidden_channels = hidden_channels
         self.mlp_hidden_dim    = mlp_hidden_dim
-        if aggregator not in ['mean', 'gcn', 'pool','max']:
-            raise ValueError(f"Unsupported aggregator: {aggregator}. Choose 'mean' or 'gcn' or 'pool'.")
+        if aggregator not in ['max', 'mean']:
+            raise ValueError(f"Unsupported aggregator: {aggregator}. Choose 'max' or 'mean'.")
         else:
             self.aggregator = aggregator
         if update_function not in ['relu', 'mlp']:
             raise ValueError(f"Unsupported update function: {update_function}. Choose 'relu' or 'mlp'.")
         else:
             self.update_function = update_function
-        
+        self.use_graph_norm = use_graph_norm
+        self.use_residuals = use_residuals
+        self.use_pos = use_pos
+
         if self.log_to_wandb:
             wandb.config.update({
                 'hidden_channels': hidden_channels,
-                'aggregator': aggregator,
-                'update_function': update_function,
-                'mlp_hidden_dim': mlp_hidden_dim,
                 'in_channels': self.in_channels,
+                'mlp_hidden_dim': mlp_hidden_dim,
+                'use_pos': use_pos,
+                'use_graph_norm': use_graph_norm,
+                'use_residuals': use_residuals,
             }, allow_val_change=True)
 
         # Define the layers of the model
@@ -82,65 +90,77 @@ class GraphSAGE(BaseGNN):
         """
         
         self.layers = nn.ModuleList()
+
         
-        if self.aggregator == 'pool':
-            self.post_concat = nn.ModuleList() #list of MLPs to concatenate the features of the source and target nodes
-        
-        for i, hidden_dim in enumerate(self.hidden_channels):
+        for i in range(len(self.hidden_channels)):
             
             if i == 0:
                 in_channels = self.in_channels
             else:
                 in_channels = self.hidden_channels[i - 1]
-            
-
+                
+            # Define the convolutional layer
             conv = SAGEConv(
-                in_channels, hidden_dim,
-                aggr=self.aggregator,   # 'mean', 'pool', 'lstm', or 'gcn'
+                in_channels, self.hidden_channels[i],
+                aggr=self.aggregator,   # 'mean', 'max', or 'lstm'
                 root_weight=True,
-                normalize=False,
-                bias=True)
-            self.layers.append(conv)
+                normalize=True,
+                bias=True,
+                project=True) #TODO: to change to True if we want to project the features to the hidden_channels[i]
+            setattr(self, f'conv{i + 1}', conv)
 
-            if self.update_function == 'mlp': 
+            if self.use_graph_norm:
+                graph_norm = GraphNorm(self.hidden_channels[i-1] if i > 0 else self.in_channels)
+                setattr(self, f'graph_norm{i + 1}', graph_norm)
+
+            if self.update_function == 'mlp': #use this if we want a second layer of non-linearity
                 # Define a small MLP for learnable update
                 update_mlp = nn.Sequential(
-                    nn.Linear(hidden_dim, self.mlp_hidden_dim),
+                    nn.Linear(self.hidden_channels[i], self.mlp_hidden_dim),
                     nn.ReLU(),
-                    nn.Linear(self.mlp_hidden_dim, hidden_dim)
+                    nn.Linear(self.mlp_hidden_dim, self.hidden_channels[i])
                 )
                 setattr(self, f'update_mlp{i + 1}', update_mlp)
-        
-        # final prediction head
-        last_dim = self.hidden_channels[-1]
-        self.fc = nn.Linear(last_dim, self.out_channels)
-        
         if self.use_dropout:
             self.dropout_layer = nn.Dropout(self.dropout)
+        
+        self.fc = nn.Linear(self.hidden_channels[-1], self.out_channels)
+        
+        
 
     def forward(self, data):
-        """
-        Forward pass with flexible aggregator and update function.
-        """
+
         # Unpack data
         x = data.x.to(self.dtype)
         edge_index = data.edge_index
         
-        if self.aggregator == 'gcn':
-            edge_index, _ = add_self_loops(edge_index, num_nodes=x.size(0))
-            
+        x = x.to(self.dtype)
         # Apply GraphSAGE layers
-        for i, conv in enumerate(self.layers):
+        for i in range(len(self.hidden_channels)):
+            x_0 = x
+                
+            conv = getattr(self, f'conv{i + 1}')
             x = conv(x, edge_index)
+            
+            # Graph normalization
+            if self.use_graph_norm:
+                graph_norm = getattr(self, f'graph_norm{i + 1}')
+                x = graph_norm(x)
+
             # apply your chosen "update function"
             if self.update_function == 'relu':
                 x = F.relu(x)
             else:
                 x = getattr(self, f'update_mlp{i + 1}')(x)
-            # optional feature dropout
+            # Dropout
             if self.use_dropout:
                 x = self.dropout_layer(x)
-        # final linear layer
+                
+            # Residual connection
+            if self.use_residuals and i > 0 and self.hidden_channels[i] == self.hidden_channels[i - 1]:
+                x = x + x_0
+
+        # Read out predictions
         return self.fc(x)
     
     '''def forward(self, data):
