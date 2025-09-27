100dbc3adeb97e77992252707b39523433bf588a
nkt1313 <ankit.basu@tum.de>
2025-09-27 11:16:33 +0200

    convert back metadata for working in retina

---
+++ convert_metadata_for_retina.py
@@ -0,0 +1,23 @@
+import json
+
+#cities = ['aschaffenburg','augsburg','bamberg','bayreuth','erlangen','fuerth','ingolstadt','kempten','landshut','muenchen','neuulm','nuernberg','regensburg','schweinfurt','wuerzburg']
+cities = ['rosenheim']
+for city in cities:
+    filepath =r'/home/abasu/gnn_predicting_effects_of_traffic_policies/data/inductive_data/training_data/kreisfreistadt/'+city+'/metadata.json'
+# Load the original file
+    with open(filepath, "r") as f:
+        data = json.load(f)
+
+    # Define the mapping from old to new base path
+    new_base = "/home/abasu/gnn_predicting_effects_of_traffic_policies/data/inductive_data/training_data/kreisfreistadt/"
+    old_base = "/dss/dssfs03/pn39mu/pn39mu-dss-0000/bavaria_simulations/inductive_data/training_data/kreisfreistadt/"
+
+    # Update the paths
+    data["path"] = [
+        path.replace(old_base, new_base) for path in data["path"]
+    ]
+
+    # Save the updated file
+    with open(filepath, "w") as f:
+        json.dump(data, f, indent=4)
+    print(f"Updated {city} metadata")
